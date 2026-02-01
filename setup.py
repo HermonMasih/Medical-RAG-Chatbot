cf6372f8e44b5fd7684cb1fc7b4a2d541cf22031
Your Name <you@example.com>
2026-02-02 03:04:39 +0530

    Initial Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Hermon Masih",
+    author_email="hermonmasih@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,10 @@
+mkdir research
+mkdir src
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/trials.ipynb
+touch setup.py
+touch app.py
+touch requirements.txt
